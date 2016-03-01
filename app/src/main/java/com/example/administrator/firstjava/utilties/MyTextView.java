a6778751f69fafaec7b9c4e9a92f611b75af0a67
Jason Lang <591678680@qq.com>
2016-03-01 21:22:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/firstjava/utilties/MyTextView.java
@@ -0,0 +1,10 @@
+package com.example.administrator.firstjava.utilties;
+
+/**
+ * Created by Administrator on 2016/3/1.
+ */
+public class MyTextView {
+    public String getValue(){
+        return "Jason's TextView: 来那个";
+    }
+}
